6a810e529a16ef129f2119734706ed3589d8a9f1
udulee <uduleenayanamalee@gmail.com>
2025-09-25 21:50:54 +0530

    reCreate bo

--- src/main/java/lk/ijse/drivingschool/bo/custom/InstructorBO.java
+++ src/main/java/lk/ijse/drivingschool/bo/custom/InstructorBO.java
@@ -1,11 +1,12 @@
 package lk.ijse.drivingschool.bo.custom;
 
+import lk.ijse.drivingschool.bo.SuperBO;
 import lk.ijse.drivingschool.dto.InstructorDTO;
 import lk.ijse.drivingschool.entity.Instructor;
 
 import java.util.List;
 
-public interface InstructorBO extends StudentBO{
+public interface InstructorBO extends SuperBO {
     List<InstructorDTO> getAllInstructor();
     boolean deleteInstructor(String instructorId);
     boolean updateInstructor(InstructorDTO instructorDTO);
--- src/main/java/lk/ijse/drivingschool/bo/custom/PaymentBO.java
+++ src/main/java/lk/ijse/drivingschool/bo/custom/PaymentBO.java
@@ -1,11 +1,12 @@
 package lk.ijse.drivingschool.bo.custom;
 
+import lk.ijse.drivingschool.bo.SuperBO;
 import lk.ijse.drivingschool.dto.PaymentDTO;
 import lk.ijse.drivingschool.entity.Payment;
 
 import java.util.List;
 
-public interface PaymentBO {
+public interface PaymentBO extends SuperBO {
     List<PaymentDTO> getAllPayment() ;
     boolean deletePayment(String id) ;
     boolean updatePayment(PaymentDTO paymentDTO) ;
